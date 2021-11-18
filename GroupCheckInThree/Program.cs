c077b05d9841e02ca9fc2a83264b2668f81141b2
Paul Lesieur <plesieur@jpkeefehs.org>
2021-11-18 11:00:36 -0500

    Add project files.

---
+++ GroupCheckInThree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GroupCheckInThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Balloons;
+            int Balls;
+            int Bananas;
+            int Bicycles;
+            int Apples;
+
+            Console.WriteLine("Hello, Class!");
+            Console.WriteLine("I have {0} Balloons, {1} Balls, {2} Bananas and {3} Bicycles");
+
+        }
+    }
+}
