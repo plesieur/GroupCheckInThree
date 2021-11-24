55d485f2a77832e1714c085ff0492d3517580d43
Paul Lesieur <plesieur@jpkeefehs.org>
2021-11-24 10:23:28 -0500

    Created a class and made variables into objects of the created class.

--- GroupCheckInThree/Program.cs
+++ GroupCheckInThree/Program.cs
@@ -4,16 +4,32 @@ namespace GroupCheckInThree
 {
     class Program
     {
+        enum Color { White, Black };
+        class Item
+        {
+
+            int _count;
+            Color _color;
+
+            public Item ()
+            {
+                _count = 0;
+                _color = Color.Black;
+            }
+        }
+
         static void Main(string[] args)
         {
-            int Balloons;
-            int Balls;
-            int Bananas;
-            int Bicycles;
-            int Apples;
+            Item Balloons = new Item();
+            Item Balls = new Item();
+            Item Marbles = new Item();
+            Item Bicycles = new Item();
+            Item Coats = new Item();
+
+            int dummy = 12;
 
             Console.WriteLine("Hello, Class!");
-            Console.WriteLine("I have {0} Balloons, {1} Balls, {2} Bananas and {3} Bicycles");
+            Console.WriteLine("I have {0} Balloons, {1} Balls, {2} Marbles and {3} Bicycles (plus {4} Coats)",dummy,dummy,dummy,dummy,dummy);
 
         }
     }
